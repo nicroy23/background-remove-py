bec69d3493f370fac02dc1c7d7c4c4bb1cd1348e
nic_roy23 <roy.nicolas4@gmail.com>
2020-08-03 22:00:54 -0400

    Refactored code into functions. Added change_background function that uses the crop function. Still needs tweaking.

--- Img.py
+++ Img.py
@@ -2,20 +2,37 @@ from PIL import Image
 from PIL import ImageColor
 import numpy as np
 import os
+import random
 
-greenScreen = Image.open('greenscreen.jpg')
 
-pix = 1, 1
+def remove_background(path):
+    green_screen = Image.open(path)
+    pix = 1, 1
+    background = 255, 255, 255, 0
+    to_see = green_screen.getpixel(pix)
+    for pixel1 in range(green_screen.width):
+        for pixel2 in range(green_screen.height):
+            current_pixel = pixel1, pixel2
+            analyzed_pixel = green_screen.getpixel(current_pixel)
+            if analyzed_pixel == to_see:
+                green_screen.putpixel(current_pixel, background)
+    #new_path = random.random() * 100 + path.split(".")[1]
+    #green_screen.save(new_path)
+    (left, upper, right, lower) = (20, 20, 100, 100)  # temporaire avant que je trouve comment mettre background transparent...
+    return green_screen.crop((left, upper, right, lower))
 
-background = 255, 255, 255, 0
 
-toSee = greenScreen.getpixel(pix)
+def change_background():
+    background_img = input("Entrez le nom de l'image de fond: ")
+    foreground_img = input("Entrez le nom de l'image en premier plan: ")
+    destination = input("Entrez le nom du nouveau fichier: ")
 
-for pixel1 in range(greenScreen.width):
-    for pixel2 in range(greenScreen.height):
-        currPixel = pixel1, pixel2
-        analyzedPixel = greenScreen.getpixel(currPixel)
-        if analyzedPixel == toSee:
-            greenScreen.putpixel(currPixel, background)
+    cropped_img = remove_background(foreground_img)
 
-greenScreen.save('test.jpg')
+    back_img = Image.open(background_img)
+    back_img.paste(cropped_img)
+
+    back_img.save(destination)
+
+
+change_background()
